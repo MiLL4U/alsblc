8fcbae652d96ea42b3bcf8abff5e21f4fe53c4bd
MiLL4U <aphiloboe@gmail.com>
2024-02-26 15:51:05 +0900

    move constants into class constants

--- alsblc/alsblcwrapper.py
+++ alsblc/alsblcwrapper.py
@@ -2,13 +2,13 @@ import numpy as np
 
 from .alsblc import AsymmetricLeastSquares as CAsymmetricLeastSquares
 
-DEFAULT_P = 0.001
-DEFAULT_ITERATION = 10
-DEFAULT_ORDER = 2
-DTYPE = np.float64
-
 
 class ALSBaselineCorrection:
+    DEFAULT_P = 0.001
+    DEFAULT_ITERATION = 10
+    DEFAULT_ORDER = 2
+    DTYPE = np.float64
+
     def __init__(self, lambda_: float,
                  p: float = DEFAULT_P,
                  iteration: int = DEFAULT_ITERATION,
@@ -39,10 +39,10 @@ class ALSBaselineCorrection:
     def get_baseline(self, spectrum: np.ndarray) -> np.ndarray:
         if spectrum.ndim != 1:
             raise TypeError("spectrum must be 1-dimensional array (vector)")
-        return self.__casymls.get_baseline(spectrum.astype(DTYPE))
+        return self.__casymls.get_baseline(spectrum.astype(self.DTYPE))
 
     def get_baselines(self, spectra: np.ndarray) -> np.ndarray:
         if spectra.ndim != 2:
             raise TypeError(
                 "spectra must be 2-dimensional array (array of vector)")
-        return self.__casymls.get_baselines(spectra.astype(DTYPE))
+        return self.__casymls.get_baselines(spectra.astype(self.DTYPE))
